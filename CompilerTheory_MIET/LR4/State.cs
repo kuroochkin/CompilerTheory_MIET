99ccf11d0599ab950f3e8c3ae8057d8eaf7212b3
kurochkinvr <pin11@kurochkin@gmail.com>
2024-12-04 17:47:56 +0300

    Завершена ЛР4

---
+++ CompilerTheory_MIET/LR4/State.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LR4
+{
+    internal class State
+    {
+        public string Name { get; }
+        public State(string name)
+        {
+            Name = name;
+        }
+
+        public static bool operator ==(State a, State b)
+        {
+            return a.Name == b.Name;
+        }
+
+        public static bool operator !=(State a, State b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is State))
+            {
+                return false;
+            }
+            return object.Equals(Name, (obj as State).Name);
+        }
+
+        public int CompareTo([AllowNull] State other)
+        {
+            return string.Compare(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ CompilerTheory_MIET/LR4/TransitionFunction.cs
@@ -0,0 +1,111 @@
+﻿namespace LR4
+{
+    internal class TransitionFunction
+    {
+        public State CurrentState { get; }
+        public State NextState { get; }
+        public string Symbol { get; }
+        public string SymbolFromH { get; }
+        public List<string> Action { get; set; }
+
+        public TransitionFunction(State currentState, State nextState, string symbol, string symbolFromH, List<string> action)
+        {
+            CurrentState = currentState;
+            NextState = nextState;
+            Symbol = symbol;
+            SymbolFromH = symbolFromH;
+            Action = action;
+        }
+
+        public override string ToString()
+        {
+            string action = "";
+            foreach (var item in Action)
+            {
+                action += item;
+            }
+            string str = "(" + CurrentState.Name + ", " + Symbol + ", " + SymbolFromH + ") = (" + NextState.Name + ", " + action + ")";
+            return str;
+        }
+
+        public static bool TryParse(string inputStr, out HashSet<TransitionFunction> transitionFunction)
+        {
+            var startState = new State("s0");
+            var emptyAction = new List<string>();
+            emptyAction.Add("`");
+
+            transitionFunction = null;
+            if (string.IsNullOrEmpty(inputStr))
+            {
+                return false;
+            }
+
+            var splitString = inputStr.Split(": ", StringSplitOptions.None);
+            if (splitString.Length != 2)
+            {
+                return false;
+            }
+
+            string symbolFromH = splitString[0].Trim(new char[] { '<', '>' });
+            var rules = splitString[1].Split(" | ");
+
+            transitionFunction = new HashSet<TransitionFunction>();
+            for (int i = 0; i < rules.Length; i++)
+            {
+                var actions = new List<string>();
+                for (int j = 0; j < rules[i].Length; j++)
+                {
+                    if (rules[i][j] == '<')
+                    {
+                        int startIndex = j + 1;
+                        while (rules[i][j] != '>')
+                        {
+                            j++;
+                        }
+                        actions.Insert(0,rules[i][startIndex..j]);
+                        continue;
+                    }
+                    if(rules[i][j] == '‘')
+                    {
+                        int startIndex = j + 1;
+                        while (rules[i][j] != '’')
+                        {
+                            j++;
+                        }
+                        var action = rules[i][startIndex..j];
+                        actions.Insert(0,action);
+
+                        TransitionFunction func2 = new TransitionFunction(startState, startState, action, action, emptyAction);
+                        transitionFunction.Add(func2);
+                        continue;
+                    }
+                    actions.Insert(0,rules[i][j].ToString());
+                }
+                if (rules[i].Length == 0)
+                {
+                    actions.Insert(0, "~");
+                }
+                TransitionFunction func = new TransitionFunction(startState, startState, "`", symbolFromH, actions);
+                transitionFunction.Add(func);
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(CurrentState, NextState, Symbol, SymbolFromH, Action);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is TransitionFunction))
+            {
+                return false;
+            }
+            return object.Equals(CurrentState, (obj as TransitionFunction).CurrentState) && object.Equals(NextState, (obj as TransitionFunction).NextState) &&
+                Action == (obj as TransitionFunction).Action && Symbol == (obj as TransitionFunction).Symbol &&
+                SymbolFromH == (obj as TransitionFunction).SymbolFromH;
+        }
+    }
+}
